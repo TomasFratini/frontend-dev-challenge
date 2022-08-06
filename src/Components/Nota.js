5d6b258355414d46286412d74e5dea4d24d46c35
Tomas Fratini <tomas.fratini09@gmail.com>
2022-08-06 00:46:56 -0300

    Entrega final

--- src/Components/Nota.js
+++ src/Components/Nota.js
@@ -6,7 +6,7 @@ const Nota = () => {
         <div>
             <img alt="nota" className="" src={comillas}/>
         </div>
-        <p className="nota-text"> Me gustaría agradecer al sitio, me ayudó de una manera inimaginable,  el psicólogo fue sumamente servicial y acogedor conmigo y me siento mejor conmigo mismo.</p>
+        <p className="nota-text"> Me gustaría agradecer al sitio, me ayudó de una manera inimaginable, el psicólogo fue sumamente servicial y acogedor conmigo y me siento mejor conmigo mismo.</p>
         <h2> ~ Maria Montessori ~</h2>
 
     </div>
